81d35c121d53d0cd441ee3c3f9f9be45852fab74
kunal885117 <kunaldasece@gmail.com>
2017-12-11 23:21:12 +0530

    working on chapter 9 exercises

---
+++ chap9_pointers_virtualfunctions/exercises/ex9_2.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+/*----------Class Declaration--------*/
+class shape{
+	protected:
+		float side1;
+		float side2;
+	public:
+		void get_data(float x, float y = 0){
+			side1 = x;
+			side2 = y;
+		}
+
+		void virtual display_area(void){
+			float area = 0;
+			cout << "Area = " << area << endl;
+		}
+};
+
+class triangle : public shape{
+	public:
+		void display_area(void){
+			float area = 0.5 * side1 * side2;
+			cout << "Area = " << area << endl;
+		}
+};
+
+class rectangle : public shape{
+	public:
+		void display_area(void){
+			float area = side1 * side2;
+			cout << "Area = " << area << endl;
+		}
+};
+
+class circle : public shape{
+	public:
+		void display_area(void){
+			float area = 3.14 * side1 * side1;
+			cout << "Area = " << area << endl;
+		}
+};
+
+/*----------Method Declaration--------*/
+int main(void){
+	//TODO
+	return 0;
+}
